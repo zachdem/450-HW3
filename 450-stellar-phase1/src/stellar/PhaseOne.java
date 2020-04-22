4b22ca6178411717e628d8ce1312c464a662df82
zachdem <51308512+zachdem@users.noreply.github.com>
2020-04-21 19:26:06 -0500

    Finished phase one

--- 450-stellar-phase1/src/stellar/PhaseOne.java
+++ 450-stellar-phase1/src/stellar/PhaseOne.java
@@ -58,6 +58,8 @@ public class PhaseOne {
 				parseCommand(command);
 			}
 		}
+		
+		s.close();
 
 	}
 
@@ -115,8 +117,9 @@ public class PhaseOne {
 	}
 
 	private static void transfer(String destinationAccount) throws IOException {
-		//double amount = Character.getNumericValue(hashStr.charAt(0)) + Character.getNumericValue(hashStr.charAt(0)) * 10
-		//		+ ((Character.getNumericValue(hashStr.charAt(2)) % 5) * 100);
+		int amount = Character.getNumericValue(hashStr.charAt(0)) + (Character.getNumericValue(hashStr.charAt(1)) * 10)
+				+ ((Character.getNumericValue(hashStr.charAt(2)) % 5) * 100);
+		System.out.println(amount);
 		
 		Network.useTestNetwork();
 		Server server = new Server("https://horizon-testnet.stellar.org");
@@ -137,7 +140,7 @@ public class PhaseOne {
 		// Start building the transaction.
 		Transaction transaction = new Transaction.Builder(sourceAccount)
 				.addOperation(
-						new PaymentOperation.Builder(destination, new AssetTypeNative(), "10").build())
+						new PaymentOperation.Builder(destination, new AssetTypeNative(), Integer.toString(amount)).build())
 				// A memo allows you to add your own metadata to a transaction. It's
 				// optional and does not affect how Stellar treats the transaction.
 				.addMemo(Memo.text("Test Transaction")).build();
@@ -201,53 +204,6 @@ public class PhaseOne {
 				System.out.println(output.toString());
 			}
 		}
-		// If some payments have already been handled, start the results from the
-		// last seen payment. (See below in `handlePayment` where it gets saved.)
-		//String lastToken = loadLastPagingToken();
-		//if (lastToken != null) {
-		//  paymentsRequest.cursor(lastToken);
-		//}
-		
-		// `stream` will send each recorded payment, one by one, then keep the
-		// connection open and continue to send you new payments as they occur.
-//		paymentsRequest.stream(new EventListener<OperationResponse>() {
-//		  @Override
-//		  public void onEvent(OperationResponse payment) {
-//		    // Record the paging token so we can start from here next time.
-//		    //savePagingToken(payment.getPagingToken());
-//			
-//		    // The payments stream includes both sent and received payments. We only
-//		    // want to process received payments here.
-//		    if (payment instanceof PaymentOperationResponse) {
-//		      if (((PaymentOperationResponse) payment).getTo().equals(keyPair)) {
-//		        return;
-//		      }
-//		      
-//		      String amount = ((PaymentOperationResponse) payment).getAmount();
-//
-//		      Asset asset = ((PaymentOperationResponse) payment).getAsset();
-//		      String assetName;
-//		      if (asset.equals(new AssetTypeNative())) {
-//		        assetName = "lumens";
-//		      } else {
-//		        StringBuilder assetNameBuilder = new StringBuilder();
-//		        assetNameBuilder.append(((AssetTypeCreditAlphaNum) asset).getCode());
-//		        assetNameBuilder.append(":");
-//		        assetNameBuilder.append(((AssetTypeCreditAlphaNum) asset).getIssuer().getAccountId());
-//		        assetName = assetNameBuilder.toString();
-//		      }
-//
-//		      StringBuilder output = new StringBuilder();
-//		      output.append(amount);
-//		      output.append(" ");
-//		      output.append(assetName);
-//		      output.append(" from ");
-//		      output.append(((PaymentOperationResponse) payment).getFrom().getAccountId());
-//		      System.out.println(output.toString());
-//		    }
-//
-//		  }
-//		});
 	}
 	
 
